5a437cf0622fd9136045f2a3ed81d753a51b0015
Diya Burman <diyaburman60@gmail.com>
2025-01-25 18:59:28 +0530

    face detection completed

--- FaceDetectionModule.py
+++ FaceDetectionModule.py
@@ -7,26 +7,46 @@ class FaceDetector():
       self.minDetectionCon = minDetectionCon
       
       self.mpFaceDetection = mp.solutions.face_detection
-      self.mpDrawing = mp.solutions.drawing_utils
-      self.faceDetection = self.mpFaceDetection.FaceDetection(0.75)
+      self.mpDraw = mp.solutions.drawing_utils
+      self.faceDetection = self.mpFaceDetection.FaceDetection(self.minDetectionCon)
 
 
     def findFaces(self, img, draw=True):
        
-       imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-       results = self.faceDetection.process(imgRGB)
-       # print(self.results)
-       bboxs = []
+      imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+      self.results = self.faceDetection.process(imgRGB)
+      # print(self.results)
+      bboxs = []
     
-       if self.results.detections:
+      if self.results.detections:
         for id, detection in enumerate(self.results.detections): 
             bboxC = detection.location_data.relative_bounding_box
             ih, iw, ic = img.shape
-            bbox = int(bboxC.xmin * iw), int(bboxC.xmin * ih), \
+            bbox = int(bboxC.xmin * iw), int(bboxC.ymin * ih), \
                    int(bboxC.width * iw), int(bboxC.height * ih)
             bboxs.append([id, bbox, detection.score])
-            cv2.rectangle(img,bbox,(255,0,255),2)
-            cv2.putText(img, f'FPS: {int(detection.score[0]*100)}%', (bbox[0],bbox[1]-20), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255), 2)
+            if draw:
+              img = self.fancyDraw(img, bbox)
+            cv2.putText(img, f'FPS: {int(detection.score[0]*100)}%', (bbox[0],bbox[1]-20), cv2.FONT_HERSHEY_PLAIN, 2, (255,0,255), 2)
+      return img, bboxs
+    
+    def fancyDraw(self, img, bbox, l=30, t=7,rt=1):
+       x, y, w, h = bbox
+       x1, y1 = x + w, y + h
+       cv2.rectangle(img,bbox,(255,0,255),rt)
+       # Top Left x,y
+       cv2.line(img, (x,y), (x+l,y),(255,0,255),t)
+       cv2.line(img, (x,y), (x,y+l),(255,0,255),t) 
+       # Top Right x1,y
+       cv2.line(img, (x1,y), (x1-l,y),(255,0,255),t)
+       cv2.line(img, (x1,y), (x1,y+l),(255,0,255),t)
+       # Bottom Left x,y1
+       cv2.line(img, (x,y1), (x+l,y1),(255,0,255),t)
+       cv2.line(img, (x,y1), (x,y1-l),(255,0,255),t) 
+       # Bottom Right x1,y1
+       cv2.line(img, (x1,y1), (x1-l,y1),(255,0,255),t)
+       cv2.line(img, (x1,y1), (x1,y1-l),(255,0,255),t) 
+       return img
 
 def main():
     cap = cv2.VideoCapture('D:\Advanced Computer Vision\Videos\convoAI.mp4')
@@ -34,7 +54,8 @@ def main():
     detector =  FaceDetector()
     while True:
       success, img = cap.read()
-      img = detector.findFaces(img)
+      img, bboxs = detector.findFaces(img)
+      print(bboxs)
       cTime = time.time()
       fps = 1/(cTime-pTime)
       pTime = cTime
